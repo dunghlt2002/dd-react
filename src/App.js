ff0672caa9d996e864749e077f87f4069f0e29ba
TriDung <dunghlt2002@gmail.com>
2020-08-24 20:22:33 -0400

    easy env boqua 4

--- src/App.js
+++ src/App.js
@@ -175,5 +175,5 @@ const mapStateToProps = (state, ownProps) => {
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
 
-// const API_URL = 'http://localhost:8080/api/';       // for local
-// const API_URL = process.env.REACT_APP_API_URL
+// REACT_APP_API_URL = https://localhost:8080/api/
+// REACT_APP_URL = https://localhost:8080/
